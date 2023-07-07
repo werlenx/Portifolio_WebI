119e9cc5dcdb5c5657cd009be5cad0af43da13ac
werlen araujo <51760214+werlenx@users.noreply.github.com>
2023-07-07 13:30:06 -0300

    pagina pre pronta

--- RepSys/js/scripts.js
+++ RepSys/js/scripts.js
@@ -2,3 +2,15 @@
 function next(){
     window.location.href = "../html/enviarAtividade.html"
 }
+function next2(){
+    window.location.href = "../html/enviarAtividade.html"
+}
+function redirecionarPagina() {
+    window.location.href = "../html/enviarAtividade2.html";
+}
+function formPronto(){
+    window.location.href = "../html/formPronto.html";
+}
+function atvddEnv(){
+    window.location.href = "../html/atividadesEnv.html";
+}
